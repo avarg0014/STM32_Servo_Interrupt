04ffd9b8c796431bcb63b4749019c8f4ff0c3e73
Your Name <you@example.com>
2025-06-02 02:38:41 -0700

    Initial commit: proyecto INTERRUPTS

--- CPP/src/main.cpp
+++ CPP/src/main.cpp
@@ -1,5 +1,4 @@
 #include "main.h"
-
 extern "C" TIM_HandleTypeDef htim15;
 
 enum PRESS_TYPE{
@@ -17,96 +16,100 @@ volatile uint32_t button_releases = 0;
 
 volatile uint32_t time_since_button_pressed = HAL_GetTick();
 
-const uint32_t NORMAL_BUTTON_PRESS_MS = 1000;
-const uint32_t LONG_BUTTONL_PRESS_MS = 2000;
-
+const uint32_t NORMAL_BUTTON_PRESS = 1000;
+const uint32_t LONG_BUTTON_PRESS = 2000;
 
 #define SERVO_TIMER htim15
 
 void maincpp(void)
 {
-
-
 	SERVO_TIMER.Instance->CCR1 = 1500;
 
 	HAL_TIM_PWM_Start(&SERVO_TIMER, TIM_CHANNEL_1);
 
 	buttonAction = PRESS_TYPE::UNDEFINED;
+
 	while(1)
 	{
-//		HAL_Delay(1);
-
 		switch(buttonAction)
 		{
-
 		case PRESS_TYPE::NORMAL:
-			if( SERVO_TIMER.Instance->CCR1 > 500 )
+			if(SERVO_TIMER.Instance->CCR1 > 500)
 			{
 				SERVO_TIMER.Instance->CCR1--;
 			}
-			else if( SERVO_TIMER.Instance->CCR1 < 500)
-			{
-					SERVO_TIMER.Instance->CCR1++;
-			}
-
-		break;
-		case PRESS_TYPE::LONG_PRESS:
-			if( SERVO_TIMER.Instance->CCR1 > 1500 )
-			{
-				SERVO_TIMER.Instance->CCR1--;
-			}
-			else if( SERVO_TIMER.Instance->CCR1 < 1500)
-			{
-				SERVO_TIMER.Instance->CCR1++;
-			}
-		break;
-		case PRESS_TYPE::SUPER_LONG_PRESS:
-			if( SERVO_TIMER.Instance->CCR1 > 2500 )
-			{
-				SERVO_TIMER.Instance->CCR1--;
-			}
-			else if( SERVO_TIMER.Instance->CCR1 < 2500)
-			{
-				SERVO_TIMER.Instance->CCR1++;
-			}
-		break;
-
-		case PRESS_TYPE::UNDEFINED:
+			if(SERVO_TIMER.Instance->CCR1 < 500)
+						{
+							SERVO_TIMER.Instance->CCR1++;
+						}
 			break;
-		}
 
+		case PRESS_TYPE::LONG_PRESS:
+			if(SERVO_TIMER.Instance->CCR1 > 1500)
+						{
+							SERVO_TIMER.Instance->CCR1--;
+						}
+						if(SERVO_TIMER.Instance->CCR1 < 1500)
+									{
+										SERVO_TIMER.Instance->CCR1++;
+									}
+						break;
+
+		case PRESS_TYPE::SUPER_LONG_PRESS:
+					if(SERVO_TIMER.Instance->CCR1 > 2500)
+								{
+									SERVO_TIMER.Instance->CCR1--;
+								}
+								if(SERVO_TIMER.Instance->CCR1 < 2500)
+											{
+												SERVO_TIMER.Instance->CCR1++;
+											}
+								break;
+
+								break;
+
+					case PRESS_TYPE::UNDEFINED:
+						break;
+		}
 	}
 }
 
-
 void HAL_GPIO_EXTI_Rising_Callback(uint16_t GPIO_Pin)
 {
-
-	if( GPIO_Pin == BUTTON_INT13_Pin)
+	if(GPIO_Pin == BUTTON_INT13_Pin)
 	{
 		uint32_t now = HAL_GetTick();
 
-		if( (now - time_since_button_pressed)  < NORMAL_BUTTON_PRESS_MS)
+		if( ( now - time_since_button_pressed) < NORMAL_BUTTON_PRESS)
 		{
 			buttonAction = PRESS_TYPE::NORMAL;
 		}
-		else if(  (now - time_since_button_pressed) < LONG_BUTTONL_PRESS_MS)
+		else if( ( now - time_since_button_pressed) < LONG_BUTTON_PRESS)
 		{
 			buttonAction = PRESS_TYPE::LONG_PRESS;
 		}
-		else{
+		else
+		{
 			buttonAction = PRESS_TYPE::SUPER_LONG_PRESS;
 		}
 		button_releases++;
 	}
 }
 
-
 void HAL_GPIO_EXTI_Falling_Callback(uint16_t GPIO_Pin)
 {
-	if( GPIO_Pin == BUTTON_INT13_Pin)
+	if(GPIO_Pin == BUTTON_INT13_Pin)
 	{
 		time_since_button_pressed = HAL_GetTick();
 		button_pressed++;
 	}
 }
+
+
+
+
+
+
+
+
+
